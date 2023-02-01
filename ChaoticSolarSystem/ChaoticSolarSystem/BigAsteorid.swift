6193eb9b2b94328fbfe93ead0df298e4e0d0027f
andy98w <113266733+andy98w@users.noreply.github.com>
2023-02-01 11:12:49 -0800

    Update BigAsteorid.swift

--- ChaoticSolarSystem/ChaoticSolarSystem/BigAsteorid.swift
+++ ChaoticSolarSystem/ChaoticSolarSystem/BigAsteorid.swift
@@ -1,10 +1,4 @@
-//
-//  BigAsteorid.swift
-//  ChaoticSolarSystem
-//
-//  Created by Xidong Wu on 8/28/15.
-//  Copyright (c) 2015 xidongwu. All rights reserved.
-//
+
 
 import SpriteKit
 class BigAsteorid : SKSpriteNode {
