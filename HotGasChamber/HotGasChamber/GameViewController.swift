bc8c840208eaeeaecfbef71a0300f695003d82a1
andy98w <113266733+andy98w@users.noreply.github.com>
2023-02-01 11:17:05 -0800

    Update GameViewController.swift

--- HotGasChamber/HotGasChamber/GameViewController.swift
+++ HotGasChamber/HotGasChamber/GameViewController.swift
@@ -1,10 +1,3 @@
-//
-//  GameViewController.swift
-//  HotGasChamber
-//
-//  Created by Xidong Wu on 8/14/15.
-//  Copyright (c) 2015 xidongwu. All rights reserved.
-//
 
 import UIKit
 import SpriteKit
